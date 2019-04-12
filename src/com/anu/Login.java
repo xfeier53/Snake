28e7b5eaafb5f1490da1b47ca4ae13b73913a378
Feier Xiao <u6609337@anu.edu.au>
2019-04-12 21:17:08 +1000

    implement register servlet and pass the test

--- src/com/anu/Login.java
+++ src/com/anu/Login.java
@@ -1,21 +1,14 @@
 package com.anu;
 import java.sql.*;
 
-public class Login {
-	// Database configuration
-	public static final String USER = "root";
-	public static final String PASSWORD = "";
-	public static final String DATABSEPORT = "3306";
-	
-    String drv = "com.mysql.cj.jdbc.Driver";
-    String url = "jdbc:mysql://localhost:" + DATABSEPORT + "/AndroidUserDB?useLegacyDatetimeCode=false&serverTimezone=Australia/Canberra";
+public class Login {	
 
     public boolean userLogin(String userID, String password){
         boolean isLoginSuccessful = false;
         String query = "SELECT * FROM AndroidUser WHERE UserID = '" + userID + "' and Password = '" + password + "'";
         try{
-            Class.forName(drv);
-            Connection conn = DriverManager.getConnection(url,USER,PASSWORD);
+            Class.forName(CONSTANTS.DRIVER);
+            Connection conn = DriverManager.getConnection(CONSTANTS.URL, CONSTANTS.USER, CONSTANTS.PASSWORD);
             Statement stm = conn.createStatement();
             ResultSet rs = stm.executeQuery(query);
             if(rs.next()){
