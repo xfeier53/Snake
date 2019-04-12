2ab5b63cbfd31ff8c6d74bb96ef72e2f32656f46
Feier Xiao <u6609337@anu.edu.au>
2019-04-12 21:41:04 +1000

    recover

---
+++ Server/src/com/anu/CONSTANTS.java
@@ -0,0 +1,9 @@
+package com.anu;
+
+public interface CONSTANTS {
+	public static final String USER = "root";
+	public static final String PASSWORD = "";
+	public static final String DATABSEPORT = "3306";
+	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	public static final String URL = "jdbc:mysql://localhost:" + CONSTANTS.DATABSEPORT + "/AndroidUserDB?useLegacyDatetimeCode=false&serverTimezone=Australia/Canberra";
+}
---
+++ Server/src/com/anu/Login.java
@@ -0,0 +1,29 @@
+package com.anu;
+import java.sql.*;
+
+public class Login {	
+
+    public boolean userLogin(String userID, String password){
+        boolean isLoginSuccessful = false;
+        String query = "SELECT * FROM AndroidUser WHERE UserID = '" + userID + "' and Password = '" + password + "'";
+        try{
+            Class.forName(CONSTANTS.DRIVER);
+            Connection conn = DriverManager.getConnection(CONSTANTS.URL, CONSTANTS.USER, CONSTANTS.PASSWORD);
+            Statement stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery(query);
+            if(rs.next()){
+            	isLoginSuccessful = true;
+            }
+            rs.close();
+            stm.close();
+            conn.close();
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+        if(isLoginSuccessful){
+            return true;
+        } else {
+        	return false;
+        }
+    }
+}
