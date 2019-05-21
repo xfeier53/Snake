412847eafadb219b99480e85cca53e39397db47e
Tao <u5527268@anu.edu.au>
2019-05-21 17:55:07 +1000

    game view edited

--- Sneak/app/src/main/java/com/example/snake/GameView.java
+++ Sneak/app/src/main/java/com/example/snake/GameView.java
@@ -95,6 +95,99 @@ public class GameView extends SurfaceView implements Runnable {
 //    }
 @Override
     public void run(){
+    while(m_Playing){
+        if(checkForUpdate()){
+            updateGame();
+            drawGame();
+        }
+    }
 
+}
+public void pause(){
+    m_Playing =false;
+    try{
+        m_Thread.join();
+    } catch(InterruptedException e){}
+}
+public void resume(){
+    m_Playing = true;
+    m_Thread = new Thread(this);
+    m_Thread.start();
+}
+public void updateGame(){
+    if(snake_x[0]==food_x&&snake_y[0]==food_y){
+        eatFood();
+    }
+    moveSnake();
+    if(detectDeath()){
+        staryGame();
+    }
+}
+public void drawGame(){
+    if(m_Holder.getSurface().isValid()){
+        canvas = m_Holder.lockCanvas();
+        canvas.drawColor(Color.argb(255,120,197,87));
+        m_Paint.setColor(Color.argb(255, 255, 255, 255));
+        m_Paint.setTextSize(30);
+        canvas.drawText("Score: "+m_score,10,30,m_Paint);
+        // snake drawing
+        for (int i = 0; i < snake_length; i++) {
+            canvas.drawRect(snake_x[i] * block_size,
+                    (snake_y[i] * block_size),
+                    (snake_x[i] * block_size) + block_size,
+                    (snake_y[i] * block_size) + block_size,
+                    m_Paint);
+        }
+        //food
+        canvas.drawRect(food_x*block_size,(food_y*block_size),(food_x*block_size)+block_size,(food_y*block_size)+block_size,m_Paint);
+        //draw
+        m_Holder.unlockCanvasAndPost(canvas);
+    }
+
+}
+public boolean checkForUpdate(){
+    if(m_NextFrameTime<=System.currentTimeMillis()){
+        m_NextFrameTime=System.currentTimeMillis()+milli_second/FPS;
+        return true;
+    }
+    return false;
+}
+public boolean onTouchEvent(MotionEvent motionEvent){
+    switch(motionEvent.getAction()&MotionEvent.ACTION_MASK){
+        case MotionEvent.ACTION_UP:
+            if(motionEvent.getX()>=screen_width/2){
+                switch (m_direction){
+                    case TOP:
+                        m_direction = SnakeDirection.RIGHT;
+                        break;
+                    case LEFT:
+                        m_direction = SnakeDirection.TOP;
+                        break;
+                    case RIGHT:
+                        m_direction = SnakeDirection.BOTTOM;
+                        break;
+                    case BOTTOM:
+                        m_direction = SnakeDirection.LEFT;
+                        break;
+                }
+            }
+            else{
+                switch (m_direction){
+                    case TOP:
+                        m_direction = SnakeDirection.LEFT;
+                        break;
+                    case LEFT:
+                        m_direction = SnakeDirection.BOTTOM;
+                        break;
+                    case RIGHT:
+                        m_direction = SnakeDirection.TOP;
+                        break;
+                    case BOTTOM:
+                        m_direction = SnakeDirection.RIGHT;
+                        break;
+                }
+            }
+            return true;
+    }
 }
 }
