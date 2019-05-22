466f6b6012478dde9d88fe20ef8e6b658c0baa32
Feier Xiao <u6609337@anu.edu.au>
2019-05-22 10:39:14 +1000

    implement all function in rank activity including get best score and records

---
+++ Server/src/com/anu/RecordServlet.java
@@ -0,0 +1,29 @@
+package com.anu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RecordServlet extends HttpServlet {
+	
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setCharacterEncoding("UTF-8");
+        
+        Record r = new Record();
+        String result = r.getRecord();
+        PrintWriter out = response.getWriter();
+        out.write(result);
+        out.flush();
+        out.close();
+    }
+ 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	doPost(request, response);
+    }
+}
