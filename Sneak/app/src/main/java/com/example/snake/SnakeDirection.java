fbdff88a1c3c401d0e9e510d3317ed1218ca3a8e
Feier Xiao <u6609337@anu.edu.au>
2019-05-23 09:06:23 +1000

    remove old test project and update SQL file

--- Sneak/app/src/main/java/com/example/snake/SnakeDirection.java
+++
@@ -1,8 +0,0 @@
-package com.example.snake;
-
-public enum SnakeDirection {
-    RIGHT,
-    LEFT,
-    TOP,
-    BOTTOM;
-}
--- initDB.sql
+++ initDB.sql
@@ -1,19 +1,33 @@
-DROP DATABASE IF EXISTS AndroidUserDB;
 CREATE DATABASE AndroidUserDB;
 USE AndroidUserDB;
 
 CREATE TABLE AndroidUser (
-	UserID varchar(100) PRIMARY KEY,
+	Account varchar(100) PRIMARY KEY,
 	Password varchar(100) NOT NULL,
-	Email varchar(100) NOT NULL
+	Email varchar(100) UNIQUE NOT NULL,
+	BestScore int DEFAULT '0'
 );
 
-SELECT * FROM AndroidUser;
+CREATE TABLE Record (
+	Account varchar(100) NOT NULL,
+	Score int DEFAULT '0'
+);
 
-INSERT INTO AndroidUser (UserID, Password, Email) VALUES 
-	("phil", "123", "xfeier53@gmail.com"),
-	("xfeier53", "456", "u6609337@anu.edu.au"),
-	("anu", "789", "anu@gmail.com");
+
+INSERT INTO Record (Account, Score) VALUES 
+	("Feier", 15),
+	("Yuan", 10),
+	("ANU", 5),
+	("Nobody4", 0),
+	("Nobody5", 0);
+
+SELECT * FROM Record;
+
+INSERT INTO AndroidUser (Account, Password, Email, BestScore) VALUES 
+	("Feier", "123", "u6609337@anu.edu.au", 15),
+	("Yuan", "456", "u6479455@anu.edu.au", 10),
+	("ANU", "789", "u1234567@anu.edu.au", 5);
+
 
 SELECT * FROM AndroidUser;
 
