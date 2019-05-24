10c70abdf86b4ac18806f0051432c7b11f04431a
Feier Xiao <u6609337@anu.edu.au>
2019-05-24 15:16:46 +1000

    testing

--- AndroidApplication/app/src/test/java/com/anu/snake/AndroidUnitTest.java
+++ AndroidApplication/app/src/test/java/com/anu/snake/AndroidUnitTest.java
@@ -3,7 +3,9 @@ package com.anu.snake;
 import android.content.Context;
 import android.graphics.Point;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
@@ -30,19 +32,73 @@ public class AndroidUnitTest {
         gv.normal_count = 0;
         gv.snake_length = 0;
         gv.m_score = 0;
+        gv.block_high = 100;
+        gv.FPS = 10;
         gv.eatFood();
         assertEquals(1, gv.normal_count);
         assertEquals(1, gv.snake_length);
         assertEquals(1, gv.m_score);
+        assertEquals(11, gv.FPS);
         gv.eatFood();
         gv.eatFood();
         assertEquals(3, gv.normal_count);
         assertEquals(3, gv.snake_length);
         assertEquals(3, gv.m_score);
+        assertEquals(13, gv.FPS);
         gv.eatFood();
         assertEquals(4, gv.normal_count);
         assertEquals(4, gv.snake_length);
         assertEquals(4, gv.m_score);
+        assertEquals(14, gv.FPS);
+    }
+
+    @Test
+    public void testSpecialFooe() {
+        gv.block_high = 100;
+        gv.normal_count = 1;
+        gv.snake_length = 1;
+        gv.m_score = 1;
+        gv.FPS = 10;
+        gv.eat_special_Food();
+        assertEquals(0, gv.normal_count);
+        assertEquals(6, gv.snake_length);
+        assertEquals(6, gv.m_score);
+        assertEquals(8, gv.FPS);
+        gv.eat_special_Food();
+        gv.eat_special_Food();
+        assertEquals(0, gv.normal_count);
+        assertEquals(16, gv.snake_length);
+        assertEquals(16, gv.m_score);
+        assertEquals(4, gv.FPS);
+    }
+
+    @Test
+    public void testSurprise() {
+        gv.block_high = 100;
+        gv.m_score = 1;
+        gv.eat_surprise();
+        assertEquals(11, gv.m_score);
+        gv.eat_surprise();
+        gv.eat_surprise();
+        assertEquals(31, gv.m_score);
+        gv.eat_surprise();
+        gv.eat_surprise();
+        assertEquals(51, gv.m_score);
+    }
+
+    @Test
+    public void testSpeed() {
+        gv.FPS = 10;
+        gv.speedUp();
+        gv.slowDown();
+        assertEquals(9, gv.FPS);
+        gv.speedUp();
+        gv.speedUp();
+        gv.speedUp();
+        assertEquals(12, gv.FPS);
+        gv.slowDown();
+        gv.slowDown();
+        assertEquals(8, gv.FPS);
     }
 
 }
